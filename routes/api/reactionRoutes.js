6c7a72e32f5557a5297318f50f191d07764cf37b
Zach Smith <zachsmith852@gmail.com>
2024-03-05 20:13:57 -0800

    Add starter for api routes

---
+++ routes/api/reactionRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const {
+  // EXAMPLES - put in variables from conroller for REACTIONS below
+  // getUsers,
+  // getSingleUser,
+  // createUser,
+} = require('../../controllers/reactionController');
+
+// update routes for reactions below
+
+// router.route('/').get(getUsers).post(createUser);
+
+// router.route('/:userId').get(getSingleUser);
+
+module.exports = router;
